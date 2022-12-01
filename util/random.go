730b38d7075aed104b555d4b713f5b909eb2b19a
Drack112 <onepessoa3@gmail.com>
2022-12-01 15:01:26 -0300

    ⚡ General Update:  SQLC generation code

---
+++ util/random.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+    "math/rand"
+    "strings"
+)
+
+const alphanet = "abcdefghijqlmnopqrstuvwxyz"
+
+func RandomInt(min, max int64) int64 {
+    return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+    var sb strings.Builder
+
+    k := len(alphanet)
+
+    for i := 0; i < n; i++ {
+        c := alphanet[rand.Intn(k)]
+        sb.WriteByte(c)
+    }
+
+    return sb.String()
+}
+
+func RandomOwner() string {
+    return RandomString(5)
+}
+
+func RandomMoney() int64 {
+    return RandomInt(1, 1000)
+}
+
+func RandomCurrency() string {
+    currencies := []string{"EUR", "BRL", "USD"}
+
+    a := len(currencies)
+    return currencies[rand.Intn(a)]
+}
