88065145affea61753b6d99667ccac2a088eebc9
Drack112 <onepessoa3@gmail.com>
2022-12-03 16:02:25 -0300

    👷 CI

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@ case "$1" in
         case "$2" in
             -c | --coverage) echo "Run only unit tests (with coverage)"
                 go test -coverpkg ./... ./... -coverprofile=coverage.txt
+                goveralls -coverprofile=coverage.txt -service semaphore -repotoken $COVERALLS_TOKEN
             ;;
             *) echo "Run only unit tests"
                 go test -v -short ./...
@@ -14,5 +15,6 @@ case "$1" in
     ;;
     *) echo "Run all tests (with coverage)"
         go test -coverpkg ./... ./... -coverprofile=coverage.txt
+        goveralls -coverprofile=coverage.txt -service semaphore -repotoken $COVERALLS_TOKEN
     ;;
 esac
