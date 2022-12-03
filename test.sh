6e171324fa0425403336ad3f4bfd1be64ece4354
Drack112 <onepessoa3@gmail.com>
2022-12-03 15:29:52 -0300

    👷 CI:  Add Coverage

--- test.sh
+++ test.sh
@@ -1,20 +1,22 @@
 case "$1" in
--s | --short)
-    case "$2" in
-      -c | --coverage) echo "Run only unit tests (with coverage)"
-          go test -v -coverprofile c.out -short ./...
-          go tool cover -html=c.out
-          ;;
-      *) echo "Run only unit tests"
-          go test -v -short ./...
-          ;;
-    esac
+    -s | --short)
+        case "$2" in
+            -c | --coverage) echo "Run only unit tests (with coverage)"
+                go test -v -coverprofile c.out -short ./...
+                go tool cover -html=c.out -o coverage.html
+                mv coverage.html /tmp/artifacts
+            ;;
+            *) echo "Run only unit tests"
+                go test -v -short ./...
+            ;;
+        esac
     ;;
--i | --integration)  echo  "Run only integration tests"
-    go test -v -run Integration ./...
+    -i | --integration)  echo  "Run only integration tests"
+        go test -v -run Integration ./...
     ;;
-*) echo "Run all tests (with coverage)"
-    go test -coverprofile c.out ./...
-    go tool cover -html=c.out
+    *) echo "Run all tests (with coverage)"
+        go test -v -coverprofile c.out -short ./...
+        go tool cover -html=c.out -o coverage.html
+        mv coverage.html /tmp/artifacts
     ;;
 esac
