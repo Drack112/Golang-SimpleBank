5f9eea7a10799c7e88721fa429e6fb6b8aea1a74
Drack <61944386+Drack112@users.noreply.github.com>
2022-12-06 12:36:59 -0300

    ⚡ General Update:  Account Controller with hashPassword

--- api/account.go
+++ api/account.go
@@ -6,6 +6,7 @@ import (
 
     db "github.com/Drack112/simplebank/db/sqlc"
     "github.com/gin-gonic/gin"
+    "github.com/lib/pq"
 )
 
 type createAccountRequest struct {
@@ -13,9 +14,9 @@ type createAccountRequest struct {
     Currency string `json:"currency" binding:"required,currency"`
 }
 
-func (server *Server) CreateAccount(ctx *gin.Context) {
+func (server *Server) createAccount(ctx *gin.Context) {
     var req createAccountRequest
-    if err := ctx.BindJSON(&req); err != nil {
+    if err := ctx.ShouldBindJSON(&req); err != nil {
         ctx.JSON(http.StatusBadRequest, ErrorResponse(err))
         return
     }
@@ -23,24 +24,30 @@ func (server *Server) CreateAccount(ctx *gin.Context) {
     arg := db.CreateAccountParams{
         Owner:    req.Owner,
         Currency: req.Currency,
+        Balance:  0,
     }
 
     account, err := server.db.CreateAccount(ctx, arg)
-
     if err != nil {
-        ctx.JSON(http.StatusBadRequest, ErrorResponse(err))
+        if pqErr, ok := err.(*pq.Error); ok {
+            switch pqErr.Code.Name() {
+            case "foreign_key_violation", "unique_violation":
+                ctx.JSON(http.StatusForbidden, ErrorResponse(err))
+                return
+            }
+        }
+        ctx.JSON(http.StatusInternalServerError, ErrorResponse(err))
         return
     }
 
     ctx.JSON(http.StatusOK, account)
-
 }
 
 type getAccountRequest struct {
     ID int64 `uri:"id" binding:"required,min=1"`
 }
 
-func (server *Server) GetAccountByID(ctx *gin.Context) {
+func (server *Server) getAccount(ctx *gin.Context) {
     var req getAccountRequest
     if err := ctx.ShouldBindUri(&req); err != nil {
         ctx.JSON(http.StatusBadRequest, ErrorResponse(err))
@@ -50,9 +57,10 @@ func (server *Server) GetAccountByID(ctx *gin.Context) {
     account, err := server.db.GetAccount(ctx, req.ID)
     if err != nil {
         if err == sql.ErrNoRows {
-            ctx.JSON(http.StatusBadRequest, ErrorResponse(err))
+            ctx.JSON(http.StatusNotFound, ErrorResponse(err))
             return
         }
+
         ctx.JSON(http.StatusInternalServerError, ErrorResponse(err))
         return
     }
@@ -65,7 +73,7 @@ type listAccountRequest struct {
     PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
 }
 
-func (server *Server) ListAccounts(ctx *gin.Context) {
+func (server *Server) listAccounts(ctx *gin.Context) {
     var req listAccountRequest
     if err := ctx.ShouldBindQuery(&req); err != nil {
         ctx.JSON(http.StatusBadRequest, ErrorResponse(err))
@@ -84,5 +92,4 @@ func (server *Server) ListAccounts(ctx *gin.Context) {
     }
 
     ctx.JSON(http.StatusOK, accounts)
-
 }
