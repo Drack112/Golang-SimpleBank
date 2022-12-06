d64cceb8c4d38bacaa60f275b32ef5dee1d405aa
Drack <61944386+Drack112@users.noreply.github.com>
2022-12-06 12:37:43 -0300

    ✅ Adding Tests: Users

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -11,9 +11,12 @@ import (
 
 func createRandomUser(t *testing.T) User {
 
+    hashedPassword, err := util.HashPassword(util.RandomString(6))
+    require.NoError(t, err)
+
     arg := CreateUserParams{
         Username:       util.RandomOwner(),
-        HashedPassword: "secret",
+        HashedPassword: hashedPassword,
         FullName:       util.RandomOwner(),
         Email:          util.RandomEmail(),
     }
