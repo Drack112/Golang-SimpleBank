18f34385603dc8e984374fcece2fad880c33845f
Drack <61944386+Drack112@users.noreply.github.com>
2022-12-11 22:59:09 -0300

    AuthMiddleware on routes

--- api/routes.go
+++ api/routes.go
@@ -5,11 +5,12 @@ import "github.com/gin-gonic/gin"
 func (server *Server) setupRouter() {
     router := gin.Default()
 
-    router.POST("/api/accounts", server.createAccount)
-    router.GET("/api/accounts/:id", server.getAccount)
-    router.GET("/api/accounts", server.listAccounts)
+    authRoutes := router.Group("/api").Use(authMiddleware(server.tokenMaker))
 
-    router.POST("/api/transfers", server.CreateTransfer)
+    authRoutes.POST("accounts", server.createAccount)
+    authRoutes.GET("accounts/:id", server.getAccount)
+    authRoutes.GET("accounts", server.listAccounts)
+    authRoutes.POST("transfers", server.CreateTransfer)
 
     router.POST("/api/users", server.createUser)
     router.POST("/api/users/login", server.loginUser)
