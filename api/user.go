6bf2c936e334b3d3527f378ac8ad27bac220b44a
Drack <61944386+Drack112@users.noreply.github.com>
2022-12-12 00:52:50 -0300

    user routes now have refresh token

--- api/user.go
+++ api/user.go
@@ -8,6 +8,7 @@ import (
     db "github.com/Drack112/simplebank/db/sqlc"
     "github.com/Drack112/simplebank/util"
     "github.com/gin-gonic/gin"
+    "github.com/google/uuid"
     "github.com/lib/pq"
 )
 
@@ -81,8 +82,12 @@ type loginRequest struct {
 }
 
 type loginUserResponse struct {
-    AccessToken string             `json:"access_token"`
-    User        createUserResponse `json:"user"`
+    SessionID             uuid.UUID          `json:"session_id"`
+    AccessToken           string             `json:"access_token"`
+    AccessTokenExpiresAt  time.Time          `json:"access_token_expires_at"`
+    RefreshToken          string             `json:"refresh_token"`
+    RefreshTokenExpiresAt time.Time          `json:"refresh_token_expires_at"`
+    User                  createUserResponse `json:"user"`
 }
 
 func (server *Server) loginUser(ctx *gin.Context) {
@@ -107,17 +112,45 @@ func (server *Server) loginUser(ctx *gin.Context) {
         return
     }
 
-    accessToken, err := server.tokenMaker.CreateToken(user.Username, server.config.AccessTokenDuration)
+    accessToken, accessPayload, err := server.tokenMaker.CreateToken(
+        user.Username,
+        server.config.AccessTokenDuration,
+    )
+    if err != nil {
+        ctx.JSON(http.StatusInternalServerError, ErrorResponse(err))
+        return
+    }
+
+    refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(
+        user.Username,
+        server.config.RefreshTokenDuration,
+    )
+    if err != nil {
+        ctx.JSON(http.StatusInternalServerError, ErrorResponse(err))
+        return
+    }
+
+    session, err := server.db.CreateSession(ctx, db.CreateSessionParams{
+        ID:           refreshPayload.ID,
+        Username:     user.Username,
+        RefreshToken: refreshToken,
+        UserAgent:    ctx.Request.UserAgent(),
+        ClientIp:     ctx.ClientIP(),
+        IsBlocked:    false,
+        ExpiresAt:    refreshPayload.ExpiredAt,
+    })
     if err != nil {
         ctx.JSON(http.StatusInternalServerError, ErrorResponse(err))
         return
     }
 
     rsp := loginUserResponse{
-        AccessToken: accessToken,
-        User:        newUserResponse(user),
+        SessionID:             session.ID,
+        AccessToken:           accessToken,
+        AccessTokenExpiresAt:  accessPayload.ExpiredAt,
+        RefreshToken:          refreshToken,
+        RefreshTokenExpiresAt: refreshPayload.ExpiredAt,
+        User:                  newUserResponse(user),
     }
-
     ctx.JSON(http.StatusOK, rsp)
-    return
 }
