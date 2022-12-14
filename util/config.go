de3fc0346ab805ca0e4e5664595f9f320ea1e65d
Drack112 <onepessoa3@gmail.com>
2022-12-13 23:36:03 -0300

    Add configurations to gRPC server

--- util/config.go
+++ util/config.go
@@ -1,32 +1,33 @@
 package util
 
 import (
-    "time"
+	"time"
 
-    "github.com/spf13/viper"
+	"github.com/spf13/viper"
 )
 
 type Config struct {
-    DBDriver             string        `mapstructure:"DB_DRIVER"`
-    DBSource             string        `mapstructure:"DB_SOURCE"`
-    ServerAddress        string        `mapstructure:"SERVER_ADDRESS"`
-    TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
-    AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
-    RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+	DBDriver             string        `mapstructure:"DB_DRIVER"`
+	DBSource             string        `mapstructure:"DB_SOURCE"`
+	HttpServerAddress    string        `mapstructure:"HTTP_SERVER_ADDRESS"`
+	GrpcServerAddress    string        `mapstructure:"GRPC_SERVER_ADDRESS"`
+	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC_KEY"`
+	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
 }
 
 func LoadConfig(path string) (config Config, err error) {
-    viper.AddConfigPath(path)
-    viper.SetConfigName("app")
-    viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
 
-    viper.AutomaticEnv()
+	viper.AutomaticEnv()
 
-    err = viper.ReadInConfig()
-    if err != nil {
-        return
-    }
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
 
-    err = viper.Unmarshal(&config)
-    return
+	err = viper.Unmarshal(&config)
+	return
 }
